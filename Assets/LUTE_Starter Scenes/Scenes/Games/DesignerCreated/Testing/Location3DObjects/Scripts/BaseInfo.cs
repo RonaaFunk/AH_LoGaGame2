bf0e5f77d3584377dc0ed645c7a7523c37dc0856
RonaaFunk <62196757+RonaaFunk@users.noreply.github.com>
2025-03-17 15:46:31 +0000

    Revert "Merge From Upstream?"
    This reverts commit c4576728b2cc0ec7d179f36e86fca3664a7813fa.

--- Assets/LUTE_Starter Scenes/Scenes/Games/DesignerCreated/Testing/Location3DObjects/Scripts/BaseInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace LoGaCulture.LUTE
-{
-    //[CreateAssetMenu(fileName = "BaseObjectInfo", menuName = "LUTE/Objects/BaseInfo", order = 0)]
-
-    public class BaseInfo : ObjectInfo
-    {
-    }
-}
