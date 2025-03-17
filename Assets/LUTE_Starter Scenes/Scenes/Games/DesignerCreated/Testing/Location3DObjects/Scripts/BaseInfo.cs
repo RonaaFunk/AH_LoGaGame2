c4576728b2cc0ec7d179f36e86fca3664a7813fa
RonaaFunk <62196757+RonaaFunk@users.noreply.github.com>
2025-03-17 15:28:59 +0000

    Merge From Upstream?

---
+++ Assets/LUTE_Starter Scenes/Scenes/Games/DesignerCreated/Testing/Location3DObjects/Scripts/BaseInfo.cs
@@ -0,0 +1,8 @@
+namespace LoGaCulture.LUTE
+{
+    //[CreateAssetMenu(fileName = "BaseObjectInfo", menuName = "LUTE/Objects/BaseInfo", order = 0)]
+
+    public class BaseInfo : ObjectInfo
+    {
+    }
+}
